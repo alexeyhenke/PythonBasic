a928e86b205fe9a0a6544ca30dce58de84a44f5b
Alexey <alexeyhenke@yandex.ru>
2020-06-11 21:59:13 +0300

    completed 6th task

--- workspace/task6.py
+++ workspace/task6.py
@@ -16,3 +16,46 @@
   6-й день: 3,22
   Ответ: на 6-й день спортсмен достиг результата — не менее 3 км
 """
+print("= " * 50)
+print("{greeting:^100}".format(greeting="Добро пожаловать в приложение составления плана тренировки!"))
+print("= " * 50)
+
+
+# число может быть дробное (дернул с stackoverflow)
+def isfloat(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+
+start_distance = 0.0
+target_distance = 0.0
+days_counter = 1
+
+while True:
+    input_value = input("Укажите дистанцию пробежки с которой Вы начнете (км): ")
+    if isfloat(input_value):
+        start_distance = int(input_value)
+        break
+    else:
+        print("Ошибка ввода: попробуйте еще раз.")
+
+while True:
+    input_value = input("Укажите дистанцию пробежки к которой Вы стремитесь (км): ")
+    if isfloat(input_value):
+        target_distance = int(input_value)
+        break
+    else:
+        print("Ошибка ввода: попробуйте еще раз.")
+
+while target_distance >= start_distance:
+    print(f' {days_counter}-й день: {start_distance:.2f}')
+    start_distance += (start_distance / 100) * 10
+    days_counter += 1
+
+start_distance += (start_distance / 100) * 10
+print(
+    f'\nОтвет: на {days_counter}-й день дистанция составит {start_distance:.2f}, '
+    f'спортсмен достигнет результата — не менее {target_distance} км')
