5c0138ce783c8c5f464c9d194a2f4a0f6bcedb4e
Alexey <alexeyhenke@yandex.ru>
2020-06-30 18:14:46 +0300

    [task 5] fix (сумму чисел в файле Строка 33)

--- workspace/task5.5.py
+++ workspace/task5.5.py
@@ -12,7 +12,7 @@ print(" =" * 50)
 
 file_name = 'file_task5.5.txt'
 file_path = path.join(path.dirname(__file__), 'files', file_name)
-summa = 0
+new_sum = 0
 file_content = ""
 
 # Создать (программно) текстовый файл
@@ -23,22 +23,19 @@ with open(file_path, 'wt', encoding='UTF-8') as file_wt:
 with open(file_path, 'at', encoding='UTF-8') as file_at:
     for el in [(lambda i: i * i)(i) for i in random.choices(range(0, 8), k=10)]:
         file_at.write(f'{el} ')
+        file_content += f'{el} '
 
     file_at.close()
 
 try:
     file = open(file_path, 'rt', encoding='UTF-8')
     print("- " * 50)
-
-    for num in file.readline().split(' '):
-        if num.isdigit():
-            file_content += num + ' '
-            summa += int(num)
+    new_sum = sum(map(int, file.read().split()))
+    file.close()
 except IOError as e:
     print(f'ERROR: {e}')
-finally:
-    file.close()
+
 
 print(f'Содержимое файла: {file_content}')
 print("- " * 50)
-print(f'Сумма всех значений находящихся в файле: {summa}')
+print(f'Сумма всех значений находящихся в файле: {new_sum}')
